043ae0e298bc5aba172739d0d17346baf16af77c
Soumik <smik1172@gmail.com>
2025-09-27 20:08:15 +0530

    add: registered slash commands to discord

--- src/registerCommands.ts
+++ src/registerCommands.ts
@@ -1,17 +1,40 @@
 import "dotenv/config";
-import { REST, Routes } from "discord.js";
-
-const commands = [
-    {
-        name: "ping",
-        description: "Checks server ping",
-    },
-];
+import { REST, Routes, SlashCommandBuilder } from "discord.js";
 
 const token = process.env.TOKEN;
-const rest = new REST({ version: "10" }).setToken(token || "");
+const applicationId = process.env.APPLICATION_ID || "";
+const guildId = process.env.GUILD_ID;
 
-async () => {
+const commands = [
+    new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Replies with server ping!"),
+    new SlashCommandBuilder().setName("help").setDescription("Show help"),
+].map((c) => c.toJSON());
+
+const rest = new REST({ version: "10" }).setToken(token || "");
+console.log("outside func");
+async function main() {
+    console.log("inside func");
     try {
-    } catch {}
-};
+        if (guildId && applicationId) {
+            console.log("Refreshing guild slash commands");
+            await rest.put(
+                Routes.applicationGuildCommands(applicationId, guildId),
+                { body: commands }
+            );
+            console.log("hitting guildid");
+        } else {
+            console.log("Refreshing global slash commands");
+            await rest.put(Routes.applicationCommands(applicationId), {
+                body: commands,
+            });
+            console.log("hitting no guildid");
+        }
+        console.log("Commands added!");
+    } catch (err) {
+        console.error("Failed to register commands");
+        process.exit(1);
+    }
+}
+main();
